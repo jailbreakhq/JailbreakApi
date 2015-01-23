fc3003cb434875ede520c837349588de61032f3b
Kevin Baker <eire32kevin@gmail.com>
2015-01-23 00:41:44 +0000

    Added heroku database configuration

--- auth-service/src/main/java/org/jailbreak/auth/AuthConfiguration.java
+++ auth-service/src/main/java/org/jailbreak/auth/AuthConfiguration.java
@@ -1,17 +1,30 @@
 package org.jailbreak.auth;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AuthConfiguration extends Configuration {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(AuthConfiguration.class);
+	
     // Database Settings
-    @JsonProperty
+    @Valid
+    @NotNull
     private DataSourceFactory database = new DataSourceFactory();
 	
+    
+    @JsonProperty("database")
     public DataSourceFactory getDataSourceFactory() {
+    	LOG.info("Dropwizard dummy DB URL will be overridden by environment variable DATABASE_URL");
+        database = HerokuDatabaseConfiguration.create();
         return database;
     }
 
