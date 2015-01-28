669edd71f71e62841d59d4c43db124f83926ab6f
Kevin Baker <eire32kevin@gmail.com>
2015-01-28 04:35:04 +0000

    Added Open CORS headers to the API

--- jailbreak-service/src/main/java/org/jailbreak/service/ServiceApplication.java
+++ jailbreak-service/src/main/java/org/jailbreak/service/ServiceApplication.java
@@ -1,5 +1,11 @@
 package org.jailbreak.service;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.jailbreak.api.representations.Representations.User;
 import org.jailbreak.service.auth.ApiTokenAuthenticator;
 import org.jailbreak.service.errors.GenericExceptionMapper;
@@ -41,6 +47,19 @@ public class ServiceApplication extends Application<ServiceConfiguration> {
     	ApiTokenAuthenticator apiTokenAuth = this.guiceBundle.getInjector().getInstance(ApiTokenAuthenticator.class);
     	environment.jersey().register(new BasicAuthProvider<User>(apiTokenAuth, "AUTH"));
     	environment.jersey().register(new GenericExceptionMapper());
+    	
+    	// Enable CORS headers
+    	Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        
+    	// Add URL mapping
+    	filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        
+    	// Configure CORS parameters
+    	filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS,HEAD");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
     }
     
     private final MigrationsBundle<ServiceConfiguration> migrations = new MigrationsBundle<ServiceConfiguration>() {
