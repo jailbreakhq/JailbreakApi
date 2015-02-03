d8c4bcc4afaa99327bdb333c54265c06cfebe366
Kevin Baker <eire32kevin@gmail.com>
2015-02-03 01:43:45 +0000

    Removed dead imports

--- jailbreak-service/src/main/java/org/jailbreak/service/db/DonationsDAO.java
+++ jailbreak-service/src/main/java/org/jailbreak/service/db/DonationsDAO.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 @RegisterMapper(DonationsMapper.class)
