ffac3e8009c71ac46a9836fa6b99b1ae3486ff6b
Kevin Baker <eire32kevin@gmail.com>
2015-03-02 03:41:10 +0000

    Wired up simple events response to resource. Fixed YT DB migration

--- jailbreak-service/src/main/java/org/jailbreak/service/base/EventsManagerImpl.java
+++ jailbreak-service/src/main/java/org/jailbreak/service/base/EventsManagerImpl.java
@@ -1,18 +1,32 @@
 package org.jailbreak.service.base;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.jailbreak.api.representations.Representations.Event;
 import org.jailbreak.api.representations.Representations.Event.EventsFilters;
 import org.jailbreak.service.core.EventsManager;
+import org.jailbreak.service.db.EventsDAO;
+import org.jailbreak.service.errors.AppException;
 
-import com.google.common.collect.Lists;
+import com.google.inject.Inject;
 
 public class EventsManagerImpl implements EventsManager {
 
+	private final EventsDAO dao;
+	
+	@Inject
+	public EventsManagerImpl(EventsDAO dao) {
+		this.dao = dao;
+	}
+	
 	@Override
 	public List<Event> getEvents(int limit, EventsFilters filters) {
-		return Lists.newArrayList();
+		try {
+			return dao.getFilteredEvents(limit, filters);
+		} catch (SQLException e) {
+			throw new AppException("Database error retrieving events", e);
+		}
 	}
 
 }
