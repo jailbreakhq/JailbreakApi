3faed9b4d0a7cbd5b46f4cfa00b395abb2aca5ff
Kevin Baker <eire32kevin@gmail.com>
2015-03-07 03:17:38 +0000

    Added create links resource and associated manager actions

--- jailbreak-service/src/main/java/org/jailbreak/service/base/events/LinkEventsManagerImpl.java
+++ jailbreak-service/src/main/java/org/jailbreak/service/base/events/LinkEventsManagerImpl.java
@@ -4,7 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+import org.jailbreak.api.representations.Representations.Event;
 import org.jailbreak.api.representations.Representations.Link;
+import org.jailbreak.api.representations.Representations.Event.EventType;
+import org.jailbreak.service.core.events.EventsManager;
 import org.jailbreak.service.core.events.LinkEventsManager;
 import org.jailbreak.service.db.dao.events.LinkEventsDAO;
 
@@ -15,10 +18,13 @@ import com.newrelic.deps.com.google.common.collect.Maps;
 public class LinkEventsManagerImpl implements LinkEventsManager {
 
 	private final LinkEventsDAO dao;
+	private final EventsManager eventsManager;
 	
 	@Inject
-	public LinkEventsManagerImpl(LinkEventsDAO dao) {
+	public LinkEventsManagerImpl(LinkEventsDAO dao,
+			EventsManager eventsManager) {
 		this.dao = dao;
+		this.eventsManager = eventsManager;
 	}
 	
 	@Override
@@ -38,6 +44,20 @@ public class LinkEventsManagerImpl implements LinkEventsManager {
 			map.put(link.getId(), link);
 		}
 		return map;
+	}
+	
+	@Override
+	public Link createLinkEvent(Link link) {
+		int newId = dao.insert(link);
+		
+		Event event = Event.newBuilder()
+				.setType(EventType.LINK)
+				.setObjectId(newId)
+				.build();
+		
+		eventsManager.createEvent(event);
+		
+		return getLinkEvent(newId).get();
 	}
 
 }
--- jailbreak-service/src/main/java/org/jailbreak/service/db/dao/events/LinkEventsDAO.java
+++ jailbreak-service/src/main/java/org/jailbreak/service/db/dao/events/LinkEventsDAO.java
@@ -5,9 +5,12 @@ import java.util.Set;
 
 import org.jailbreak.api.representations.Representations.Link;
 import org.jailbreak.service.db.BindIds;
+import org.jailbreak.service.db.BindProtobuf;
 import org.jailbreak.service.db.mappers.events.LinkEventsMapper;
 import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 import org.skife.jdbi.v2.sqlobject.customizers.SingleValueResult;
 
@@ -16,11 +19,15 @@ import com.google.common.base.Optional;
 @RegisterMapper(LinkEventsMapper.class)
 public interface LinkEventsDAO {	
 	
-	@SqlQuery("SELECT * FROM events_link WHERE AND id = :id")
+	@SqlUpdate("INSERT INTO events_link (url, link_text, description, photo_url) VALUES (:url, :link_text, :description, :photo_url)")
+	@GetGeneratedKeys
+	public abstract int insert(@BindProtobuf Link link);
+	
+	@SqlQuery("SELECT * FROM events_link WHERE id = :id")
 	@SingleValueResult(Link.class)
 	Optional<Link> getLink(@Bind("id") int id);
 	
 	@SqlQuery("SELECT * FROM events_link WHERE id = ANY (:idList)")
 	List<Link> getLinks(@BindIds Set<Integer> ids);
-	
+
 }
