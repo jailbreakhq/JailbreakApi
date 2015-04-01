9d632729abe87cb00d9c7021a888e2ace2cfb5e0
Kevin Baker <eire32kevin@gmail.com>
2015-04-01 14:56:41 +0100

    API error field name fix

--- jailbreak-service/src/main/java/org/jailbreak/service/errors/ErrorMessage.java
+++ jailbreak-service/src/main/java/org/jailbreak/service/errors/ErrorMessage.java
@@ -4,19 +4,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ErrorMessage {
 	
+	// don't change the case of properties as they are the API response field names
+	
 	@JsonProperty
-	private int status;
+	private int statusCode;
 	
 	@JsonProperty
 	private String message;
 	
 	@JsonProperty
-	private String docs_link;
+	private String docsLink;
 	
 	public ErrorMessage(int status, String message, String docs_link) {
-		this.status = status;
+		this.statusCode = status;
 		this.message = message;
-		this.docs_link = docs_link;
+		this.docsLink = docs_link;
 	}
 	
 }
